8f71de304bcc921cd1d5ad8b3179ffaae6b0abcc
Paolo Josue Coaquira Anccori <paolojosuecoaquiraanccori@gmail.com>
2025-08-23 12:27:01 -0500

    Editando datos para que no aparesca error en las tildes

--- hospital.sql
+++ hospital.sql
@@ -43,14 +43,14 @@ CREATE TABLE Recetas (
     FOREIGN KEY (IdMedicamento) REFERENCES Medicamentos(IdMedicamento)
 );
 
-INSERT INTO Medicos (Nombre, Especialidad) VALUES ('Dr. Juan Pérez', 'Cardiología');
-INSERT INTO Medicos (Nombre, Especialidad) VALUES ('Dra. María López', 'Pediatría');
+INSERT INTO Medicos (Nombre, Especialidad) VALUES ('Dr. Juan Perez', 'Cardiologia');
+INSERT INTO Medicos (Nombre, Especialidad) VALUES ('Dra. Maria Lopez', 'Pediatria');
 
-INSERT INTO Pacientes (Nombre, DNI, Telefono) VALUES ('Carlos Gómez', '12345678', '999111222');
+INSERT INTO Pacientes (Nombre, DNI, Telefono) VALUES ('Carlos Gomez', '12345678', '999111222');
 INSERT INTO Pacientes (Nombre, DNI, Telefono) VALUES ('Ana Torres', '87654321', '988776655');
 
-INSERT INTO Medicamentos (Nombre, Tipo, Stock) VALUES ('Paracetamol', 'Analgésico', 200);
-INSERT INTO Medicamentos (Nombre, Tipo, Stock) VALUES ('Amoxicilina', 'Antibiótico', 150);
+INSERT INTO Medicamentos (Nombre, Tipo, Stock) VALUES ('Paracetamol', 'Analgesico', 200);
+INSERT INTO Medicamentos (Nombre, Tipo, Stock) VALUES ('Amoxicilina', 'Antibiotico', 150);
 
 INSERT INTO Citas (IdPaciente, IdMedico) VALUES (1, 1);
 INSERT INTO Citas (IdPaciente, IdMedico) VALUES (2, 2);
